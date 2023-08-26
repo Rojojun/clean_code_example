2670944cc185713d61f863241bf90769d0c91f97
rojojun <zoloman316@gmail.com>
2023-08-26 13:23:19 +0900

    ArgsException getter, setter, 생성자 추가

--- src/main/java/cleancode/ArgsException.java
+++ src/main/java/cleancode/ArgsException.java
@@ -1,5 +1,58 @@
 package cleancode;
 
+import static cleancode.ArgsException.ErrorCode.*;
+
 public class ArgsException extends Exception{
     private char errorArgumentId = '\0';
+    private String errorParameter = null;
+    private ErrorCode errorCode = OK;
+
+    public ArgsException() {}
+
+    public ArgsException(String message) {
+        super(message);
+    }
+
+    public ArgsException(ErrorCode errorCode, String errorParameter) {
+        this.errorCode = errorCode;
+        this.errorParameter = errorParameter;
+    }
+
+    public ArgsException(ErrorCode errorCode, char errorArgumentId, String errorParameter) {
+        this.errorCode = errorCode;
+        this.errorArgumentId = errorArgumentId;
+        this.errorParameter = errorParameter;
+    }
+
+    public char getErrorArgumentId() {
+        return errorArgumentId;
+    }
+
+    public String getErrorParameter() {
+        return errorParameter;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorArgumentId(char errorArgumentId) {
+        this.errorArgumentId = errorArgumentId;
+    }
+
+    public void setErrorParameter(String errorParameter) {
+        this.errorParameter = errorParameter;
+    }
+
+    public void setErrorCode(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public enum ErrorCode {
+        OK,
+        INVALID_ARGUMENT_FORMAT,
+        UNEXPECTED_ARGUMENT,
+        INVALID_ARGUMENT_NAME,
+
+    }
 }
